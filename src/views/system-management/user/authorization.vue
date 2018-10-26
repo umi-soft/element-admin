3a758ec63699b02905a42caa011405831f0a8030
梁娟 <liangjuan@mail.taiji.com.cn>
2018-10-26 16:49:52 +0800

    用户管理模块批量对用户分配部门

--- src/views/system-management/user/authorization.vue
+++ src/views/system-management/user/authorization.vue
@@ -22,12 +22,44 @@
         </el-table-column>
       </el-table>
     </el-card>
+    <el-card>
+      <el-row>
+        <el-col :span="24">
+          <el-card>
+            <el-input v-model="filter" placeholder="输入关键字进行过滤"/>
+          </el-card>
+        </el-col>
+      </el-row>
+      <template slot="header">
+        <button-right>
+          部门信息
+          <template slot="button">
+            <el-button type="primary" @click="submitUserDeptHandler()" >授权</el-button>
+          </template>
+        </button-right>
+      </template>
+      <el-tree :data="[{}]" :props="defaultProps">
+        <div slot-scope="{ data }" class="custom-tree-node">
+          <div class="name">名称</div>
+          <div class="time">最后修改时间</div>
+          <div class="time">创建时间</div>
+        </div>
+      </el-tree>
+      <el-tree ref="tree" :data="depts" :props="defaultProps" :filter-node-method="filterNodeHandler" class="filter-tree" highlight-current accordion show-checkbox @current-change="(value, node) => selected = value">
+        <div slot-scope="{ data }" class="custom-tree-node">
+          <div class="name">{{ data.name }}</div>
+          <div class="time">{{ data.modifiedDate | parseTime }}</div>
+          <div class="time">{{ data.createdDate | parseTime }}</div>
+        </div>
+      </el-tree>
+    </el-card>
   </div>
 </template>
 
 <script>
 import * as UserAPI from '@/api/system-management/user'
 import * as RoleAPI from '@/api/system-management/role'
+import * as DeptAPI from '@/api/system-management/dept'
 import mixins from './mixins'
 
 export default {
@@ -41,8 +73,19 @@ export default {
   },
   data() {
     return {
+      filter: null,
       roles: [],
-      selectedRoles: []
+      selectedRoles: [],
+      depts: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  watch: {
+    filter(filter) {
+      this.$refs.tree.filter(filter)
     }
   },
   activated() {
@@ -52,6 +95,9 @@ export default {
     }
     RoleAPI.queryAllRoles(params).then(data => {
       this.roles = data
+    })
+    DeptAPI.queryAllTreeDepts({}).then(data => {
+      this.depts = data
     })
   },
   methods: {
@@ -91,6 +137,45 @@ export default {
           message: '已取消授权'
         })
       })
+    },
+    submitUserDeptHandler() {
+      const selectedDepts = this.$refs.tree.getCheckedNodes()
+      if (selectedDepts.length === 0) {
+        this.$message({
+          type: 'error',
+          message: '您未勾选任何部门'
+        })
+        return
+      }
+      this.$confirm('此操作将批量授权, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '已授权成功'
+        })
+        const userDepts = []
+        this.detail.forEach(user => {
+          selectedDepts.forEach(dept => {
+            userDepts.push({
+              userId: user.id,
+              deptId: dept.id
+            })
+          })
+        })
+        UserAPI.addUserDept(userDepts)
+      }).catch(() => {
+        this.$message({
+          type: 'warning',
+          message: '已取消授权'
+        })
+      })
+    },
+    filterNodeHandler(value, data) {
+      if (!value) return true
+      return data.name.indexOf(value) !== -1
     }
   }
 }
@@ -98,11 +183,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .el-card {
+    margin-top: 10px;
+  }
   /deep/ .el-card {
     border: none;
   }
   .query-btn /deep/ .el-button {
     float: right;
     margin-left: 10px;
+  }
+  /deep/ .el-tree {
+    border-bottom: 1px solid #ebeef5;
+
+    .el-tree-node__content {
+      border-top: 1px solid #ebeef5;
+      border-left: 1px solid #ebeef5;
+      border-right: 1px solid #ebeef5;
+      min-height: 40px;
+    }
+  }
+
+  .custom-tree-node {
+    width: 100%;
+
+    .name {
+      float: left;
+      min-height: 40px;
+      line-height: 40px;
+    }
+
+    .time {
+      float: right;
+      width: 200px;
+      min-height: 40px;
+      line-height: 40px;
+      border-left: 1px solid #ebeef5;
+      text-align: center;
+    }
+
   }
 </style>
