1dddb92c4c1a722c6448a40cbe10e23c3cb4f299
hujie <hujie@17luma.com>
2019-01-14 15:09:10 +0800

    解决webpack-dev-server升级问题引起的host检测异常，https://github.com/webpack/webpack-dev-server/issues/1604

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -33,6 +33,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     historyApiFallback: true,
     hot: true,
     compress: true,
+    disableHostCheck: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
